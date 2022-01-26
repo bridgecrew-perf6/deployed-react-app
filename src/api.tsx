57fc62290bb7b4e70bdbaa44fd72ce57ff06d7c3
Alessandro Colombo <alessandro.colombo13@gmail.com>
2022-01-26 00:22:05 -0800

    some develop

---
+++ src/api.tsx
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+async function makeRequest(q: any) {
+  return true;
+}
+const api = {
+  fetchPlacesByCoords: (lat: any, log: any) => makeRequest(''),
+};
+export default api;
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://places-service-copy-8lw5s0fq.uc.gateway.dev',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
